1a9098ef88fceb90f3e466ffb0bc708202486b2a
Jianli Zhang <jianli.zh42g@gmail.com>
2023-09-23 20:38:48 +1000

    update readme and add help

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# jumper
+# Jumper
+
+A command line tool for switching folders.
+
+## How to use
+
+- Installation
+
+  ```
+  ./install.sh
+  ```
+
+- Display the manual
+  ```
+  j --help
+  ```
--- jumper.sh
+++ jumper.sh
@@ -1,11 +1,33 @@
-#!/bin/bash
+#! /bin/bash
+
+function help() {
+   # Display Help
+   echo "Command line tool for switching folders."
+   echo
+   echo "Commands: "
+   echo "j          :Switch to the base working directory."
+   echo "j  <Directory Name>"
+   echo "           :Switch to the target directory."
+   echo "jadd <Directory Name> <Directory Path>"
+   echo "           :Register a new folder with its path."
+   echo "jassemble  <Directory Name>"
+   echo "           :Assemble the target directory and register it"
+   echo "jalias <Shortcut>  <Registered Directory Name>"
+   echo "           :Register the given shortcut as the target directory."
+   echo
+}
 
 function jump() {
-  if [ -z "$1" ]; then
+  target=$1
+  if [ -z "${target}" ]; then
     # shellcheck disable=SC2164
     echo -e "GOTO $JUMPER_WORKSPACE"
     cd "${JUMPER_WORKSPACE}"
   else
+    if [ "${target}" == "--help" ]; then
+        help
+        exit 0
+    fi
     JUMPER=$JUMPER_HOME/jumper
     FIRST_DIR=$($JUMPER goto "$1" | tr -d '"')
     
