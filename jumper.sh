e7b89efb37f36b963f838e85c75396c570e293d8
Jianli Zhang <jianli.zh42g@gmail.com>
2023-09-29 10:19:04 +1000

    fix bug of help command exit

--- jumper.sh
+++ jumper.sh
@@ -25,25 +25,25 @@ function jump() {
     cd "${JUMPER_WORKSPACE}"
   else
     if [[ "${target}" == "--help" ]]; then
-        help
-        exit 0
-    fi
-    JUMPER=$JUMPER_HOME/jumper
-    FIRST_DIR=$($JUMPER goto "${target}" | tr -d '"')
-    
-    if (( $(grep -c . <<<"${FIRST_DIR}") > 1 )); then
-      echo -e "${FIRST_DIR}\n"
-    fi
-
-    FIRST_DIR=$(echo "${FIRST_DIR}" | tail -n 1)
-    echo -e "GOTO: $FIRST_DIR"
-
-    if [[ -d $FIRST_DIR ]]; then
-      # shellcheck disable=SC2164
-      cd "$FIRST_DIR"
+      help
     else
-      FIRST_DIR=$1
-      echo -e "\n$FIRST_DIR is not a valid directory"
+      JUMPER=$JUMPER_HOME/jumper
+      FIRST_DIR=$($JUMPER goto "${target}" | tr -d '"')
+      
+      if (( $(grep -c . <<<"${FIRST_DIR}") > 1 )); then
+        echo -e "${FIRST_DIR}\n"
+      fi
+
+      FIRST_DIR=$(echo "${FIRST_DIR}" | tail -n 1)
+      echo -e "GOTO: $FIRST_DIR"
+
+      if [[ -d $FIRST_DIR ]]; then
+        # shellcheck disable=SC2164
+        cd "$FIRST_DIR"
+      else
+        FIRST_DIR=$1
+        echo -e "\n$FIRST_DIR is not a valid directory"
+      fi
     fi
   fi
 }
